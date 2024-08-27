f6c1b336926d71f8d7b97345c6b3c75d24de54b7
aozturk1 <aozturk1@asu.edu>
2024-08-26 20:01:07 -0700

    Added a simple react hook to display current existing tickets

--- bugtracker-app/src/App.js
+++ bugtracker-app/src/App.js
@@ -1,22 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [tickets, setTickets] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('api/tickets') // Update this to match your Spring Boot endpoint for fetching tickets
+      .then(response => response.json())
+      .then(data => {
+        setTickets(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-intro">
+          <h2>Ticket List</h2>
+          {tickets.map(ticket =>
+            <div key={ticket.id}>
+              {ticket.title} - {ticket.description}
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
